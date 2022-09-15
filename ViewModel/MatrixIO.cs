c5105fd8df58373a9b04604b3fbd01779c5b44a5
bertbsky <bertbsky@localhost>
2022-09-15 15:00:04 +0200

    add more examples using converters

--- ViewModel/MatrixIO.cs
+++ ViewModel/MatrixIO.cs
@@ -84,7 +84,7 @@ namespace RouterControlButtonDesigner.ViewModel
                 }
             }
         }
-                      
+
         private bool _isSelected;
         public bool IsSelected
         {
